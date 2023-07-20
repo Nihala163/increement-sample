08ad03d255edc72321533540165042897780eecd
softroniics-calicut <softroniicscalicut@gmail.com>
2023-07-20 13:07:41 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/pg1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pg1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  bool _increment = true;
+
+  void _incrementCounter() {
+    setState(() {
+      if (_increment) {
+        _counter++;
+        {
+          if (_counter == 10) {
+            _increment = false;
+          }
+        }
+      }
+      else {
+        _counter--;
+        {
+          if (_counter == 0) {
+            _increment = true;
+          }
+        }
+      }
+
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
